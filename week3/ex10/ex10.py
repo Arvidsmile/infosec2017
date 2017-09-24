952b7fbd9350053a5d60bf1a456e18e9002d71da
Arvid Lindstrom <arvid.lindstrom@gmail.com>
2017-09-24 21:01:02 +0200

    shiggedi giggedi

---
+++ week3/ex10/ex10.py
@@ -0,0 +1,19 @@
+# A beetle has 4 legs
+# A spider has 6 legs
+# No spiders or beetles are mutilated
+# 56 = 4*b + 6*s
+
+# Reference to pages that helped with this:
+# https://brilliant.org/wiki/linear-diophantine
+# -equations-one-equation/
+
+#1. Observation 1
+legs = 56 / 2
+# 28 = 2b + 3s
+
+# 28 is NOT divisible by 3 so we must have at 
+# least 1 beetle which gives us 24 legs
+# left to solve for
+# 24 = 2b + 3s 
+
+# 
