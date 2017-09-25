e09bb7b1beae3b582db98ec5feac2250692d1d9b
SoftLipsHardNips <nilstoltanso@gmail.com>
2017-09-25 02:32:11 +0200

    Finished 6 :)

--- week2/ex6/ex6.py
+++ week2/ex6/ex6.py
@@ -1,9 +1,6 @@
 import sys
 import hashlib
 
-K1 = ''
-K2 = ''
-
 def readFile(name):
 	file = open(name,'r')
 	return file.read()
@@ -12,61 +9,20 @@ def sha256 ( input ) :
 	return (hashlib.sha256(str(input)\
 		.encode('utf -8 ')).hexdigest())
 
-def blockText(text):
+def processText(text):
 	blockedText = []
 	#Change text to binary and append to list
 	for i in range(len(text)):
-		# blockedText.append(Base10toBinary(ord(text[i])))
 		blockedText.append(ord(text[i]))
 	#Create padding at the end of list so 
 	#as to make length multiple of 8
 	if len(text)%8 != 0:
 		#Create marker (byte 128)
-		#blockedText.append(Base10toBinary(128))
 		blockedText.append(128)
 		for i in range(8 - (len(text)%8+1)):
 			#pad rest of block with byte 0
-			# blockedText.append(Base10toBinary(0))
 			blockedText.append(0)
 	return blockedText
-
-def feistel(blockedText, round, encryptMode):
-	ciphertext = []
-	#Determine key
-	key = []
-	if round < 8:
-		for i in range(4):
-			key.append(int(K1[round*4+i],16))
-	else:
-		for i in range(4):
-			key.append(int(K2[round*4+i],16))
-	#Encrypt
-	for i in range(len(blockedText)/8):
-		newLeft = []
-		newRight = []
-		newBlock = []
-		for j in range(4):
-			#New Left-side
-			if encryptMode == True:
-				#if encrypting
-				#add latter 4 bytes (right side) to new left side
-				ciphertext.append(blockedText[i*8+(4+j)])
-			else:
-				#if decrypting
-				#xor right side with key and add to new left side
-				xorValue =  blockedText[i*8+(4+j)] ^ key[j]
-				ciphertext.append(xorValue)
-		for j in range(4):
-			if encryptMode == True:
-				#if encrypting
-				#xor left side with key and add to new right side
-				xorValue = blockedText[i*8+(j)] ^ key[j]
-				ciphertext.append(xorValue)
-			else:
-				#if decrypting
-				#add first 4 bytes (left side) to new right side
-				ciphertext.append(blockedText[i*8+j])		
-	return ciphertext
 
 def from_bytes (data, big_endian = False):
     if isinstance(data, str):
@@ -80,8 +36,6 @@ def from_bytes (data, big_endian = False):
 
 def intToChar(blockedText):
 	text = ''
-	print 'Blocked Text'
-	print blockedText
 	for i in range(len(blockedText)):
 		text = text + (chr(blockedText[i]))
 	return text
@@ -97,29 +51,64 @@ def bytes_from_file(filename):
 		inputFile.close()
 	return array
 
+def feistel(blockedText, round, totalRounds, encryptMode, K1):
+	ciphertext = []
+	#Determine key
+	key = []
+	if encryptMode == True:
+		#Read key forwards if encrypting
+		for i in range(0,4):
+			hexa = K1[(round*8)+(i*2)] + K1[(round*8)+(i*2)+1]
+			key.append(int(hexa,16))
+	else:
+		#Read key backwards in steps of 4 if decrypting 
+		for i in range(0,4):
+			hexa = K1[(totalRounds -1 - round)*8+(i*2)] + \
+				K1[(totalRounds -1 - round)*8+(i*2)+1]
+			key.append(int(hexa,16))
+	#Encrypt
+	for i in range(len(blockedText)/8):
+		for j in range(4):
+			#New Left-side
+			if encryptMode == True:
+				#if encrypting
+				#add latter 4 bytes (right side) to new left side
+				ciphertext.append(blockedText[i*8+(4+j)])
+			else:
+				#if decrypting
+				#xor right side with key and add to new left side
+				xorValue =  blockedText[i*8+(4+j)] ^ key[j]
+				ciphertext.append(xorValue)
+		for j in range(4):
+			#New Right side
+			if encryptMode == True:
+				#if encrypting
+				#xor left side with key and add to new right side
+				xorValue = blockedText[i*8+(j)] ^ key[j]
+				ciphertext.append(xorValue)
+			else:
+				#if decrypting
+				#add first 4 bytes (left side) to new right side
+				ciphertext.append(blockedText[i*8+j])		
+	return ciphertext
+
 def main():
 	key = raw_input("Key: ")
-	print key
-	K1 = sha256(key)
-	print K1
-	K2 = sha256(K1)
-	print K2
+	K1 = sha256(key) + sha256(sha256(key))
 	rounds = input("Num of rounds: ")
 	encrypt = raw_input("1 == Encrypt, 0 == Decrypt : ")
 	if encrypt == '1':
 		encryptMode = True
 		text = readFile(sys.argv[1])
+		blockedText = processText(text);
+		print 'Original text: '
 		print text
-		blockedText = blockText(text);
 	else:
 		encryptMode = False
 		blockedText = bytes_from_file(sys.argv[1])
-		print blockedText
-	
 	for i in range(rounds):
-		blockedText = feistel(blockedText, i, encryptMode)
-	#Change from binary to String character
-	# finalText = binaryArrayToString(blockedText)
+		blockedText = feistel(blockedText, i, rounds, encryptMode, K1)
+	#Change from int to String character
 	finalText = intToChar(blockedText) 
 	print 'Final Text: '
 	print finalText
