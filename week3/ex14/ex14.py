2da17002433666d6361612fed96865b86423225e
Arvid Lindstrom <arvid.lindstrom@gmail.com>
2017-10-01 15:36:35 +0200

    shiggedi giggedi

--- week3/ex14/ex14.py
+++ week3/ex14/ex14.py
@@ -21,9 +21,11 @@ def ellipticCurveAddition(p1, p2, a, mod):
 	#1. Calculate 'm'
 	m = 0
 	if(isEqual(p1, p2)):
-		m = (3 * pow(p1[0], 2) + a) * modinv(2 * p1[1], mod) % mod
+		m = (3 * pow(p1[0], 2) + a) * \
+			modinv(2 * p1[1], mod) % mod
 	else:
-		m = (p2[1] - p1[1]) * modinv((p2[0] - p1[0]), mod) % mod
+		m = (p2[1] - p1[1]) * \
+			modinv((p2[0] - p1[0]), mod) % mod
 
 	x3 = 0
 	y3 = 0
@@ -34,10 +36,10 @@ def ellipticCurveAddition(p1, p2, a, mod):
 
 	return (x3, y3)
 
-m = int(raw_input("multiplier: "))
-a = int(raw_input("a: "))
-mod = int(raw_input("modulus: "))
-point = (int(raw_input("pointX: ")), int(raw_input("pointY: ")))
+# m = int(raw_input("multiplier: "))
+# a = int(raw_input("a: "))
+# mod = int(raw_input("modulus: "))
+# point = (int(raw_input("pointX: ")), int(raw_input("pointY: ")))
 
 # On curve : y^2 = x^3 + ax + b % modulus
 # given multiplier (scalar) and a point(Xp, Yp)
@@ -45,8 +47,36 @@ point = (int(raw_input("pointX: ")), int(raw_input("pointY: ")))
 def ellipticCurveMult(multiplier, point, a, modulus):
 	increment = point
 	for i in range(multiplier - 1):
+		print point
 		point = ellipticCurveAddition(point, increment, a, modulus)
 	return point
 
-print "15 * (2, 7) = " + str(ellipticCurveMult(m, point, a, mod))
+# Fast multiplication 
+
+a = 11
+point = (2, 7)
+m = 15
+mod = 167
+
+print "15 * 2, 7) = " + str(ellipticCurveMult(m, point, a, mod))
+
+# Find high bit points
+# Given a multiplier and a point, compute all
+# additions and return a list of the ones corresponding
+# to the position with a high-bit in the multiplier
+def findHighBitPoints():
+	pass
+
+# Given a list of all points which had a high bit
+def recAdd(a, mod, list):
+	print list
+	if(len(list) == 2):
+		return ellipticCurveAddition(list[0], list[1], a, mod)
+	else:
+		point = list[0]
+		return ellipticCurveAddition(\
+			recAdd(a, mod, list[1:]), point, a, mod)	
+
+print "Recursive addition"
+print recAdd(11, 167, [P1, P2, P3, P4])
 
